30c514e05f777feacbeb26889cb9e77f8a08b0e0
nasim0994 <nasimuddin0994@gmail.com>
2025-01-26 18:36:43 +0600

    products section design

---
+++ frontend/src/components/main/home/Products.tsx
@@ -0,0 +1,23 @@
+import ProductCard from "../ProductCard/ProductCard";
+
+export default function Products() {
+  return (
+    <section className="pb-10">
+      <div className="container">
+        <div className="text-center">
+          <h1 className="text-xl font-medium uppercase text-neutral md:text-2xl md:font-semibold">
+            Ready for Order
+          </h1>
+
+          <p className="mt-2 text-sm text-neutral-content">
+            Discover the Best Deals on Your Favorite Cars
+          </p>
+        </div>
+
+        <div className="mt-8 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+          <ProductCard />
+        </div>
+      </div>
+    </section>
+  );
+}
--- frontend/src/pages/main/Home.tsx
+++ frontend/src/pages/main/Home.tsx
@@ -1,11 +1,13 @@
 import ChooseByCategory from "@/components/main/home/ChooseByCategory";
 import Hero from "@/components/main/home/Hero";
+import Products from "@/components/main/home/Products";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <ChooseByCategory />
+      <Products />
     </>
   );
 }
